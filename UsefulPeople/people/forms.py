0655c538206d79c16a7e089806080d78e9cbe0d0
JigSaW <misho_1998@mail.ru>
2021-07-15 17:43:28 +0300

    auth

--- UsefulPeople/people/forms.py
+++ UsefulPeople/people/forms.py
@@ -3,9 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 from .models import Users
 
 class UsersForm(forms.ModelForm):
-    login = forms.CharField(
-        widget = forms.TextInput(attrs={'class': "input_log"}),
-        )
+
     class Meta:
         model = Users
         fields = ('login', 'password',)
@@ -13,3 +11,12 @@ class UsersForm(forms.ModelForm):
             'login': _('Логин'),
             'password': _('Пароль')
         }
+
+    def __init__(self, *args, **kwargs):
+        super(UsersForm, self).__init__(*args, **kwargs)
+        self.fields['login'].widget = forms.TextInput(attrs={
+            'class': 'input_log',
+            })
+        self.fields['password'].widget = forms.TextInput(attrs={
+            'class': 'input_passwd',
+            })
