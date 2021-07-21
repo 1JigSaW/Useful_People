da65dc81231338b51bba76c1df1512666b1808a7
JigSaW <misho_1998@mail.ru>
2021-07-21 23:06:52 +0300

    users:

--- UsefulPeople/people/views.py
+++ UsefulPeople/people/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import UsersForm, UsersRegistrationForm
 from django.http import Http404, HttpResponse, HttpResponseRedirect
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 
 def index(request):
     return render(request, 'index.html')
@@ -12,6 +12,7 @@ def registration(request):
         user_form = UsersRegistrationForm(request.POST)
         if user_form.is_valid():
             new_user = user_form.save()
+            new_user.active = True
             return render(request, 'registration_done.html', {'new_user': new_user})
         else:
             form = UsersRegistrationForm()
@@ -30,16 +31,21 @@ def authorisation(request):
         form = UsersForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(login=cd['login'], password=cd['password'])
-            if user.is_active:
-                login(request, user)
-                errors.append('Вы успешно зарегестрированы')
-                return render(request, 'registration_done.html', {'form': form,
-                    'errors': errors})
-            else:
-                errors.append('Несуществующий аккаунт')
-                return HttpResponse('Disabled account')
+            username = request.POST.get('login')
+            password = request.POST.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return render(request, 'registration_done.html', {'form': form,
+                        'errors': errors})
+                else:
+                    errors.append('Несуществующий аккаунт')
+                    return HttpResponse('Disabled account')
 
+            else:
+                errors.append('Неправильый логин')
+                return HttpResponse('Invalid login')
         else:
             errors.append('Введите корректные данные')
             form = UsersForm()
